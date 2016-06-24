e60e9f054fa28f3576f5d86c009c5b6cbc6f0cbc
seanet7or <mail@becait.de>
2016-06-24 22:21:42 +0200

    Packages update

--- fsRemote/fsRemote.UWP/MainPage.xaml.cs
+++ fsRemote/fsRemote.UWP/MainPage.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
